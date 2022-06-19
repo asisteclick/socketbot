a3a96b00bcbf11edc765b331f5eb1127040fa334
AsisteClick <info@asisteclick.com>
2022-06-19 15:18:00 -0300

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,4 +1,5 @@
 <?php
+	header('Access-Control-Allow-Origin: *');
     $phone = $_GET['phone'];
     $aspid = $_GET['aspid']; 
     $lang = $_GET['lang'];
@@ -11,28 +12,25 @@
     <meta charset="utf-8" /> 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <link rel="stylesheet" href="https://app.asisteclick.com/css/bootstrap.min.css"/> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"/> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body> 
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js"></script>
-    <script defer src="<?=$BASE_URL?>/js/bootstrap.min.js"></script>
 
     <!-- a bootstrap input field with a submit button to the right -->
     <div class="container">
         <div class="row">
 			<div class="col-md-12" style="margin-bottom: 20px;">
 				<h1>AsisteClick Socket-Client Bot</h1>	
-				Ejemplo de adaptador de sockets en Javascript para el control de una conversación con un bot. Este bot habla en español y puede conversar varios tópicos, responder consultas científicas, realizar cálculos, buscar datos históricos y muchos más. 
+				Ejemplo de adaptador de sockets en Javascript para el control de una conversación con un bot. Este bot habla en español y puede conversar varios tópicos, responder consultas científicas, realizar cálculos, buscar datos históricos, contar chistes y muchos más. 
+				Github: <a href="https://github.com/asisteclick/socketbot" target="_blank">https://github.com/asisteclick/socketbot</a>.
 			</div>
 			<div class="col-md-12">
 				<div class="input-group">
 					<input id="message" type="text" class="form-control" placeholder="Mensaje al bot" disabled>
 					<div class="input-group-btn">
 						<button id="send_message" class="btn btn-primary" type="submit">
-						&nbsp;&nbsp;<i class="fa fa-paper-plane" aria-hidden="true"></i>
+						&nbsp;&nbsp;<i class="fa fa-paper-plane" aria-hidden="true"></i>&nbsp;&nbsp;
 						</button>
 					</div>
 				</div>
@@ -41,12 +39,16 @@
 		<div id="messages" class="row"></div>
     </div>
 
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"></script>
+
     <script>
     var session_id = null;
     var session_token = null; 
 
     // connects socket
-    var socket = io.connect("https://desa-sockets.asisteclick.com", {transports: ['websocket']});  
+    var socket = io.connect("https://sockets.asisteclick.com", {transports: ['websocket']});  
     log("Iniciando sesión...");
     
     // socket connected
@@ -75,7 +77,7 @@
     // socket listens to bot messages
     socket.on('agent says', function(data) {
 		$("#message").prop("disabled", false);
-        log("Bot:", data.msg);
+        log("Bot:", data.msg, data);
     });
 
     // sends a message to the bot
@@ -93,11 +95,13 @@
     });
 
     // log conversation to the screen
-    function log(who, what) { 
-        what = (what) ? what : "";
+    function log(who, what, data) { 
+		what = (what) ? what : "";
+		data = (data) ? `<br/><span style="color:lightgray">${JSON.stringify(data)}</span>` : "";
         $("#messages").prepend(`
         	<div class="col-md-12" style="margin-top: 20px;">
                 <b>${who}</b> ${what}
+				${data}
             </div>
         `);
     }
